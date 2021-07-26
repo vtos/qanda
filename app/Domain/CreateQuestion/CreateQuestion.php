c778fe625b1a14580b9304dda45a9bc4533e635e
Vitaly Potenko <potenkov@gmail.com>
2021-07-26 12:24:39 +0300

    Implemented command-handler pattern for questions.

--- app/Domain/CreateQuestion/CreateQuestion.php
+++ app/Domain/CreateQuestion/CreateQuestion.php
@@ -1,10 +1,31 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Domain\CreateQuestion;
 
-
-class CreateQuestion
+/**
+ * Class to represent a DTO (Data Transfer Object) to contain data required for question creation.
+ */
+final class CreateQuestion
 {
+    private string $questionText;
 
+    private string $questionAnswer;
+
+    public function __construct(string $questionText, string $questionAnswer)
+    {
+        $this->questionText = $questionText;
+        $this->questionAnswer = $questionAnswer;
+    }
+
+    public function questionText(): string
+    {
+        return $this->questionText;
+    }
+
+    public function questionAnswer(): string
+    {
+        return $this->questionAnswer;
+    }
 }
--- app/Domain/CreateQuestion/CreateQuestionHandler.php
+++ app/Domain/CreateQuestion/CreateQuestionHandler.php
@@ -1,10 +1,25 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Domain\CreateQuestion;
 
+use App\Models\Question;
 
-class CreateQuestionHandler
+final class CreateQuestionHandler
 {
+    private Question $questionModel;
 
+    public function __construct(Question $questionModel)
+    {
+        $this->questionModel = $questionModel;
+    }
+
+    public function handle(CreateQuestion $createQuestion): void
+    {
+        $this->questionModel->question_text = $createQuestion->questionText();
+        $this->questionModel->question_answer = $createQuestion->questionAnswer();
+
+        $this->questionModel->save();
+    }
 }
